abb6c37c5f007533b3466367beb9a089cbff026c
c5kua0 <miobokopolo@gmail.com>
2026-08-20 12:07:30 +0800

    Update TreeCommand.java

--- src/main/java/me/reno/treecapitator/TreeCommand.java
+++ src/main/java/me/reno/treecapitator/TreeCommand.java
@@ -1,43 +1,64 @@
 package me.reno.treecapitator;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
-import java.util.List;
+public class TreeCommand implements CommandExecutor {
 
-public class ChadAxeCommand implements CommandExecutor, TabCompleter {
+    private final TreeChadItem chadItem;
 
-    private final TreeCapitator plugin;
-
-    public ChadAxeCommand(TreeCapitator plugin) {
-        this.plugin = plugin;
+    public TreeCommand(TreeChadItem chadItem) {
+        this.chadItem = chadItem;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can use this command.");
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(
+                    Component.text("Only players can use this command.")
+                            .color(NamedTextColor.RED)
+            );
+
             return true;
         }
 
-        Player player = (Player) sender;
+        if (!player.hasPermission("treecapitator.chad")) {
 
-        if (!player.hasPermission("treecapitator.give")) {
-            player.sendMessage("You don't have permission to do that.");
+            player.sendMessage(
+                    Component.text("You don't have permission to use /chad.")
+                            .color(NamedTextColor.RED)
+            );
+
             return true;
         }
 
-        player.getInventory().addItem(ChadAxeItem.create());
-        player.sendMessage("You have received the Chad Axe.");
+        player.getInventory().addItem(
+                chadItem.createChad()
+        );
+
+        player.sendMessage(
+                Component.text("You received ")
+                        .color(NamedTextColor.YELLOW)
+                        .append(
+                                Component.text("Chad")
+                                        .color(NamedTextColor.GOLD)
+                        )
+                        .append(
+                                Component.text("!")
+                                        .color(NamedTextColor.YELLOW)
+                        )
+        );
+
         return true;
-    }
-
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return Collections.emptyList();
     }
 }
