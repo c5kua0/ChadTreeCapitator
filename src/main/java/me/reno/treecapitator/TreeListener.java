374362cb95fd2f7d24315f8e3ca835d37de71cbe
c5kua0 <miobokopolo@gmail.com>
2026-08-20 10:53:37 +0800

    Create TreeListener.java

---
+++ src/main/java/me/reno/treecapitator/TreeListener.java
@@ -0,0 +1,12 @@
+package me.reno.treecapitator;
+
+import org.bukkit.event.Listener;
+
+public class TreeListener implements Listener {
+
+    private final TreeCapitatorPlugin plugin;
+
+    public TreeListener(TreeCapitatorPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
