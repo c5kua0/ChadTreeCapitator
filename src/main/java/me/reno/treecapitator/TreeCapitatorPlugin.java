4bae5a15358e4033952e88f745d58b927fc3365c
c5kua0 <miobokopolo@gmail.com>
2026-08-20 11:25:34 +0800

    Update TreeCapitatorPlugin.java

--- src/main/java/me/reno/treecapitator/TreeCapitatorPlugin.java
+++ src/main/java/me/reno/treecapitator/TreeCapitatorPlugin.java
@@ -1,26 +1,33 @@
 package me.reno.treecapitator;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TreeCapitatorPlugin extends JavaPlugin {
+public final class TreeCapitator extends JavaPlugin {
+
+    public static final NamespacedKey CHAD_AXE_KEY = new NamespacedKey("treecapitator", "chad_axe");
+    private static TreeCapitator instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
 
-        getCommand("chadaxe").setExecutor(
-                new ChadAxeCommand()
-        );
+        getServer().getPluginManager().registerEvents(new TreeChopListener(this), this);
 
-        getServer().getPluginManager().registerEvents(
-                new TreeListener(this),
-                this
-        );
+        ChadAxeCommand chadAxeCommand = new ChadAxeCommand(this);
+        getCommand("chadaxe").setExecutor(chadAxeCommand);
+        getCommand("chadaxe").setTabCompleter(chadAxeCommand);
 
-        getLogger().info("Chad Tree Capitator enabled!");
+        getLogger().info("TreeCapitator enabled.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Chad Tree Capitator disabled!");
+        getLogger().info("TreeCapitator disabled.");
+    }
+
+    public static TreeCapitator getInstance() {
+        return instance;
     }
 }
