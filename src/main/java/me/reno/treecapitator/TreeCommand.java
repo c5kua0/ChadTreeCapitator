0039c7e5ba71938014174fdfc73ae39c940824eb
c5kua0 <miobokopolo@gmail.com>
2026-08-20 11:26:03 +0800

    Update TreeCommand.java

--- src/main/java/me/reno/treecapitator/TreeCommand.java
+++ src/main/java/me/reno/treecapitator/TreeCommand.java
@@ -1,45 +1,43 @@
 package me.reno.treecapitator;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
-public class ChadAxeCommand implements CommandExecutor {
+import java.util.Collections;
+import java.util.List;
+
+public class ChadAxeCommand implements CommandExecutor, TabCompleter {
+
+    private final TreeCapitator plugin;
+
+    public ChadAxeCommand(TreeCapitator plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
-    public boolean onCommand(
-            CommandSender sender,
-            Command command,
-            String label,
-            String[] args
-    ) {
-
-        if (!(sender instanceof Player player)) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage("Only players can use this command.");
             return true;
         }
 
-        ItemStack axe = new ItemStack(Material.NETHERITE_AXE);
+        Player player = (Player) sender;
 
-        ItemMeta meta = axe.getItemMeta();
-
-        if (meta != null) {
-            meta.setDisplayName(ChatColor.GOLD + "Chad");
-            axe.setItemMeta(meta);
+        if (!player.hasPermission("treecapitator.give")) {
+            player.sendMessage("You don't have permission to do that.");
+            return true;
         }
 
-        player.getInventory().addItem(axe);
-
-        player.sendMessage(
-                ChatColor.GREEN + "You received the " +
-                ChatColor.GOLD + "Chad Axe!"
-        );
-
+        player.getInventory().addItem(ChadAxeItem.create());
+        player.sendMessage("You have received the Chad Axe.");
         return true;
     }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return Collections.emptyList();
     }
+}
